cf8d883df903d3dfe485b1fff385664d2829087b
Muhammad Kevin Hardiansyah <141954008+kevinhardiansites@users.noreply.github.com>
2024-02-23 02:17:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def extract_transcript_details(youtube_video_url):
         video_id=youtube_video_url.split("=")[1]
         print(video_id)
         transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
-        
+
         transcript = ""
         for i in transcript_text:
             transcript += "" + i["text"]
@@ -36,14 +36,14 @@ def generate_gemini_content(transcript_text,prompt):
 
 
 st.title("Youtube Video Summarizer")
-youtube_video_url= st.text_input("Enter Youtube Video Link:")
+youtube_link= st.text_input("Enter Youtube Video Link:")
 
-if youtube_video_url:
-    video_id=youtube_video_url.split("=")[1]
+if youtube_link:
+    video_id=youtube_link.split("=")[1]
     print(video_id)
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 if st.button("Get Important Notes"):
-      transcript_text=extract_transcript_details(youtube_video_url)
+      transcript_text=extract_transcript_details(youtube_link)
 
       if transcript_text:
           summary=generate_gemini_content(transcript_text,prompt)
