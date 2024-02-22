6ec206ef33808099996922ddeef6200018087411
Muhammad Kevin Hardiansyah <141954008+kevinhardiansites@users.noreply.github.com>
2024-02-23 01:26:12 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,51 +1,59 @@
 import streamlit as st
 from dotenv import load_dotenv
 
-load_dotenv() #load all env variables
+load_dotenv() ##load all the nevironment variables
 import os
 import google.generativeai as genai
-from youtube_transcript_api import YouTubeTranscriptApi
 
+from youtube_transcript_api import YouTubeTranscriptApi
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-prompt="""You are Youtube video summarizer. You will be taking the transcript text and summarizing teh entire video and providing the important summary in points within 250 words. Please provide the summary of the text given here: """
+prompt="""You are Yotube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
 
-#getting transcript data from youtube videos
+
+## getting the transcript data from yt videos
 def extract_transcript_details(youtube_video_url):
     try:
         video_id=youtube_video_url.split("=")[1]
-        print(video_id)
+        
         transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
 
         transcript = ""
         for i in transcript_text:
-            transcript += "" + i["text"]
+            transcript += " " + i["text"]
 
         return transcript
 
     except Exception as e:
         raise e
-
-#getting summary based on prompt from gemini pro
+    
+## getting the summary based on Prompt from Google Gemini Pro
 def generate_gemini_content(transcript_text,prompt):
 
     model=genai.GenerativeModel("gemini-pro")
     response=model.generate_content(prompt+transcript_text)
     return response.text
 
-
-st.title("Youtube Video Summarizer")
-youtube_link= st.text_input("Enter Youtube Video Link:")
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
 
 if youtube_link:
-    video_id=youtube_link.split("=")[1]
+    video_id = youtube_link.split("=")[1]
     print(video_id)
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
-if st.button("Get Important Notes"):
-      transcript_text=extract_transcript_details(youtube_link)
 
-      if transcript_text:
-          summary=generate_gemini_content(transcript_text,prompt)
-          st.markdown('##Important Notes')
-          st.write(summary)
+if st.button("Get Detailed Notes"):
+    transcript_text=extract_transcript_details(youtube_link)
+
+    if transcript_text:
+        summary=generate_gemini_content(transcript_text,prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
+
+
+
+
+
